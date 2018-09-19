8eac1453d84b73b7a5df43d28f33601313b11de4
MariaSopkova <lisica.m@mail.ru>
2018-09-19 23:17:23 +0300

    Files as params

--- src/ru/innopolis/stc/Main.java
+++ src/ru/innopolis/stc/Main.java
@@ -1,23 +1,35 @@
 package ru.innopolis.stc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        try {
+            String[] files = getFilesPath(args[0]);
+            String[] pattrns = patters(args[1]);
+            getOccurencies(files, pattrns, args[2]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public static void getOccurencies(String[] sources, String[] words, String res) throws InterruptedException {
         System.out.println(System.currentTimeMillis());
         long start = System.currentTimeMillis();
-        String path = "file:///C:\\Users\\Maria\\Desktop\\Бжехва Ян. Академия пана Кляксы - royallib.ru.txt";
-        String hpPath = "http://www.glozman.com/TextPages/Harry%20Potter%207%20-%20Deathly%20Hollows.txt";
-        String big_file = "file:///D:\\innopolis\\test_set\\aa1bed85-aca5-4fff-8b4b-a133ea4e0ca9.txt";
         ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
         ConcurrentLinkedQueue<String> queueWithPatterns = new ConcurrentLinkedQueue<>();
 
-        String[] str = {big_file, hpPath};
-        FilesReaderThread readerThread = new FilesReaderThread(queue, str);
+        FilesReaderThread readerThread = new FilesReaderThread(queue, sources);
         readerThread.start();
-        String[] patter = {"well"};
-        PatternFinderThread finderThread = new PatternFinderThread(queue, queueWithPatterns, patter);
+
+        PatternFinderThread finderThread = new PatternFinderThread(queue, queueWithPatterns, words);
         finderThread.start();
         QueueWriterThread writer = new QueueWriterThread(queueWithPatterns, "result.txt");
         writer.start();
@@ -35,5 +47,33 @@ public class Main {
         writer.join();
         System.out.println("work time: " + (System.currentTimeMillis() - start));
         System.out.println(System.currentTimeMillis());
+    }
+
+    public static String[] getFilesPath(String fileInfoPath) throws IOException {
+        List<String> files = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileInfoPath)));
+        String directory = reader.readLine();
+        File folder = new File(directory);
+        File[] filesInDirectory = folder.listFiles();
+        for (File entry : filesInDirectory) {
+            if (!entry.isDirectory()) {
+                files.add("file:///" + entry.getPath());
+            }
+        }
+        String filePath;
+        while ((filePath = reader.readLine()) != null) {
+            files.add(filePath);
+        }
+        return (String[]) files.toArray();
+    }
+
+    public static String[] patters(String fileName) throws IOException {
+        BufferedReader pattrnsReader = new BufferedReader(new FileReader(new File(fileName)));
+        String pattern;
+        List<String> patterns = new ArrayList<>();
+        while ((pattern = pattrnsReader.readLine()) != null) {
+            patterns.add(pattern);
+        }
+        return (String[]) patterns.toArray();
     }
 }
